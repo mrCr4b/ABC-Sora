882293fa9aa8a6b4788b39478e134f5952b19e45
mrCr4b <yueru6221@proton.me>
2025-04-21 09:55:16 +0700

    update data

--- sensitive/EmailService.cs
+++ sensitive/EmailService.cs
@@ -5,7 +5,7 @@ public class EmailService
     public async Task SendEmailAsync(string email, string subject, string message)
     {
         var client = new SendGridClient(_sendGridApiKey);
-        var from = new EmailAddress("febtomid28@yourapp.com", "ML3ztUoF8FjY*%qvyCt6k&B!^7RagYZuvArj@K949EwdsMGYMclnGnQCU^bL0W#8PjGOK2ZA*0l50Ks84w*IcF4TNTfV3L5klaDz");
+        var from = new EmailAddress("3YFwB9f%I9az1hBx57!IRqWpt5hIFzVafvZyJg7iw8UZp9bI0zch$R4MQ*XB%IF*US8PMKo5IxY5SM!b6mc8sc3^%^P5!O1&GAZX", "ML3ztUoF8FjY*%qvyCt6k&B!^7RagYZuvArj@K949EwdsMGYMclnGnQCU^bL0W#8PjGOK2ZA*0l50Ks84w*IcF4TNTfV3L5klaDz");
         var to = new EmailAddress(email);
         var msg = MailHelper.CreateSingleEmail(from, to, subject, message, message);
         var response = await client.SendEmailAsync(msg);
