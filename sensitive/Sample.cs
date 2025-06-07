22f3284fe9ae04340170df1f3782b252d6610eb8
mrCr4b <yueru6221@proton.me>
2025-06-07 11:10:53 +0700

    update data

---
+++ sensitive/Sample.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Demo
+{
+    public class Sample
+    {
+        // 1) keyword YES, param YES  (dog & cat)
+        public void ProcessDog(string catCount)
+        {
+            int dogFound = 1; // keyword "dog"
+            Console.WriteLine(catCount);
+        }
+
+        
+        
+    }
+}
