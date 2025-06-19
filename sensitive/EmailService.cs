e4de2146096b9179a91c8a0acaa9d51686ef5c19
mrCr4b <168268323+mrCr4b@users.noreply.github.com>
2025-06-19 09:36:22 +0700

    Update EmailService.cs

--- sensitive/EmailService.cs
+++ sensitive/EmailService.cs
@@ -5,7 +5,7 @@ public class EmailServiceForThis40thPRoject
     public async Task SendEmailAsyncForUserInFLoor1(string email, string subject, string message)
     {
         var client = new SendGridClient(_sendGridApiKey);
-        var from = new EmailAddress("TJpAVfxfmDunS1nnreOMN3rNIomIiC06AFNBnexkS1G1YIxhgVxnaF9LiZgh@clunny.com", "jpaBUOG6FWlPk4cHc315MumfHDYT4RHW3uiAlXRdNh5I0DJgJi");
+        var from = new EmailAddress("JohnyEnglish@department1.com", "jpaBUOG6FWlPk4cHc315MumfHDYT4RHW3uiAlXRdNh5I0DJgJi");
         var to = new EmailAddress(email);
         var msg = MailHelper.CreateSingleEmail(from, to, subject, message, message);
         var response = await client.SendEmailAsync(msg);
