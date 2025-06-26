7122581b9755b5c8c5f4dac01f70e3b2d112a2af
mrCr4b <yueru6221@proton.me>
2025-06-26 14:14:10 +0700

    Update data

--- sensitive/Sample_2.java
+++ sensitive/Sample_2.java
@@ -7,7 +7,7 @@ public class Sample {
 
     // ─────────────────────────────────────────────────────────────
     // 1)  keyword YES, param YES   →  "keyword yes param yes"
-    public void uploadProcess(String tokenSecret) {
+    public void initProcess(String configSecret) {
         // llm
 		int money = 99;
         int jackpot = 0;
